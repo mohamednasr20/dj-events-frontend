78bdc028d00d5eb96581be3b75f666b723643109
mohamednasr20 <mohamednasr8687@gmail.com>
2022-02-20 00:07:22 -0500

    display events

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,21 @@
 import Layout from '@/components/Layout';
 import { API_URL } from '@/config/index';
+import EventItem from '@/components/EventItem';
 import Link from 'next/link';
 
 export default function Home({ events }) {
   return (
     <Layout>
       <h1>Upcoming Events</h1>
-      <Link href="/about">About</Link>
+      {events.length === 0 && <h3>No Events To Show</h3>}
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      )}
     </Layout>
   );
 }
@@ -17,8 +26,8 @@ export async function getStaticProps() {
 
   return {
     props: {
-      events,
-      revalidate: 1,
+      events: events.slice(0, 3),
     },
+    revalidate: 1,
   };
 }
