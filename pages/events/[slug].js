b6d16265c8c7108beee00ac84b31340839c1d353
mohamednasr20 <mohamednasr8687@gmail.com>
2022-03-03 22:48:09 -0500

    add event detalies page

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,20 +1,42 @@
 import Layout from '@/components/Layout';
 import { useRouter } from 'next/router';
 import { API_URL } from '@/config/index';
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from '@/styles/Event.module.css';
 
-export default function EventPage() {
-  const router = useRouter();
+export default function EventPage({ evt }) {
   return (
     <Layout>
-      <h1>My Event</h1>
-      <h3>{router.query.slug}</h3>
-      <button onClick={() => router.push('/')}>Click</button>
+      <div className={styles.event}>
+        <span>
+          {new Date(evt.date).toLocaleDateString('en-US')} at {evt.time}
+        </span>
+        <h1>{evt.name}</h1>
+
+        {evt.image && (
+          <div className={styles.image}>
+            <Image src={evt.image} width={960} height={600} />
+          </div>
+        )}
+
+        <h3>Performers:</h3>
+        <p>{evt.performers}</p>
+        <h3>Description:</h3>
+        <p>{evt.description}</p>
+        <h3>Venue: {evt.venue}</h3>
+        <p>{evt.address}</p>
+
+        <Link href="/events">
+          <a className={styles.back}>{'<'} Go Back</a>
+        </Link>
+      </div>
     </Layout>
   );
 }
 
 export async function getServerSideProps({ query: { slug } }) {
-  const res = await fetch(`${API_URL}/events?slug=${slug}`);
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
   const events = await res.json();
 
   return {
